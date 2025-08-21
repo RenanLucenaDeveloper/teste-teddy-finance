ad31d3331ca3f7ca2c370e470878bd2e2a574dc1
RenanLucenaDeveloper <Reenanluceena@gmail.com>
2025-08-21 18:58:35 -0300

    Zustand SelectedClients Store

---
+++ src/store/selectedClientsStore.ts
@@ -0,0 +1,34 @@
+import { create } from "zustand";
+import { createJSONStorage, persist } from 'zustand/middleware'
+import type { User } from "../types/user-type";
+
+export type selectedClientsState = {
+    selectedClients: User[];
+    select: (client: User) => void;
+    remove: (client: User) => void;
+    clearAll: () => void;
+};
+
+export const useSelectedClientsStore = create<selectedClientsState>()(
+    persist<selectedClientsState>(
+        (set) => ({
+            selectedClients: [],
+
+            select: (client) => set((state) => 
+                state.selectedClients.some((item) => client.id === item.id)
+                ? state 
+                : { selectedClients: [client] }
+            ),
+
+            remove: (client) => set((state) => ({
+                selectedClients: state.selectedClients.filter((item) => client.id !== item.id)
+            })),
+
+            clearAll: () => set(() => ({ selectedClients: [] })),
+        }),
+        {
+            name: 'selectedClients',
+            storage: createJSONStorage(() => localStorage)
+        }
+    )
+);
